cd388b19910dad895bdfb663a40342e31f51a5cd
VictoriyaRoy <victoriyaroi2003@gmail.com>
2022-10-20 13:19:49 +0300

    Fix base app

--- core/src/androidTest/java/com/viktoriiaroi/core/ExampleInstrumentedTest.kt
+++ core/src/androidTest/java/com/viktoriiaroi/core/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.viktoriiaroi.core", appContext.packageName)
+        assertEquals("com.viktoriiaroi.core.test", appContext.packageName)
     }
 }
