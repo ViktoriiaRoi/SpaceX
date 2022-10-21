38908b5f295bd0a8491a5a782ac34c9a35b8040b
VictoriyaRoy <victoriyaroi2003@gmail.com>
2022-10-21 14:51:16 +0300

    Add navigation

--- app/src/main/java/com/viktoriiaroi/spacex/MainActivity.kt
+++ app/src/main/java/com/viktoriiaroi/spacex/MainActivity.kt
@@ -2,10 +2,20 @@ package com.viktoriiaroi.spacex
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupNavigation()
+    }
+
+    fun setupNavigation() {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        val navView: BottomNavigationView = findViewById(R.id.bottom_nav_view)
+        navView.setupWithNavController(navController)
     }
 }
