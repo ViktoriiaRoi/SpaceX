55f0859467bf93d7dc6783e2a8195a22b7aa6ef2
VictoriyaRoy <victoriyaroi2003@gmail.com>
2022-10-21 13:16:23 +0300

    Add splash screen

--- app/src/main/java/com/viktoriiaroi/spacex/MainActivity.kt
+++ app/src/main/java/com/viktoriiaroi/spacex/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_SpaceX)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
