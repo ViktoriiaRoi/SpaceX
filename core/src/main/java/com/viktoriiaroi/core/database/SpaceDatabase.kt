8c1705db6b22536f491f88d9a068f36428c310b4
VictoriyaRoy <victoriyaroi2003@gmail.com>
2022-10-24 17:15:59 +0300

    Setup Room

---
+++ core/src/main/java/com/viktoriiaroi/core/database/SpaceDatabase.kt
@@ -0,0 +1,7 @@
+package com.viktoriiaroi.core.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+// @Database(entities = [], version = 1, exportSchema = true)
+abstract class SpaceDatabase : RoomDatabase()
---
+++ core/src/main/java/com/viktoriiaroi/core/di/DatabaseModule.kt
@@ -0,0 +1,29 @@
+package com.viktoriiaroi.core.di
+
+import android.content.Context
+import androidx.room.Room
+import com.viktoriiaroi.core.database.SpaceDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class DatabaseModule {
+    companion object {
+        private const val DB_NAME = "spacex.db"
+    }
+
+    @Provides
+    @Singleton
+    fun provideSpaceDatabase(@ApplicationContext appContext: Context): SpaceDatabase {
+        return Room.databaseBuilder(
+            appContext,
+            SpaceDatabase::class.java,
+            DB_NAME
+        ).build()
+    }
+}
