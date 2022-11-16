8e75d1e9fa26bef4a409a855070dc45f0222e9ad
VictoriyaRoy <victoriyaroi2003@gmail.com>
2022-11-16 15:17:43 +0200

    Add design of launches

--- app/src/main/java/com/viktoriiaroi/spacex/utils/BindingAdapters.kt
+++ app/src/main/java/com/viktoriiaroi/spacex/utils/BindingAdapters.kt
@@ -3,8 +3,12 @@ package com.viktoriiaroi.spacex.utils
 import android.content.Intent
 import android.net.Uri
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import coil.load
+import com.viktoriiaroi.spacex.R
 
 class BindingAdapters {
     companion object {
@@ -17,6 +21,24 @@ class BindingAdapters {
                     ContextCompat.startActivity(view.context, browserIntent, null)
                 }
             }
+        }
+
+        @BindingAdapter("android:loadImage")
+        @JvmStatic
+        fun loadImage(imageView: ImageView, path: String?) {
+            if (path == null) return
+            imageView.load(path) {
+                placeholder(R.drawable.default_launch)
+                error(R.drawable.default_launch)
+            }
+        }
+
+        @BindingAdapter("android:joinList")
+        @JvmStatic
+        fun joinList(textView: TextView, list: List<String>) {
+            textView.text =
+                if (list.isEmpty()) "None"
+                else list.joinToString(separator = ", ")
         }
     }
 }
--- app/src/main/java/com/viktoriiaroi/spacex/utils/DateUtils.kt
+++ app/src/main/java/com/viktoriiaroi/spacex/utils/DateUtils.kt
@@ -1,5 +1,6 @@
 package com.viktoriiaroi.spacex.utils
 
+import com.viktoriiaroi.core.model.DatePrecision
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -7,8 +8,29 @@ object DateUtils {
     private val dateFormatter = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
     private val timeFormatter = SimpleDateFormat("hh:mm", Locale.ENGLISH)
 
-    fun timestampToDate(timestamp: Long): Pair<String, String> {
-        val date = Date(timestamp * 1000)
-        return Pair(dateFormatter.format(date), timeFormatter.format(date))
-    }
+    private val dayDateFormatter = SimpleDateFormat("MMMM d, yyyy", Locale.ENGLISH)
+    private val monthDateFormatter = SimpleDateFormat("MMMM yyyy", Locale.ENGLISH)
+    private val yearDateFormatter = SimpleDateFormat("yyyy", Locale.ENGLISH)
+    private val precisionTimeFormatter = SimpleDateFormat("hh:00 a z", Locale.ENGLISH)
+
+    fun getDate(timestamp: Int) = Date(timestamp.toLong() * 1000)
+
+    fun unixToDate(timestamp: Int): String = dateFormatter.format(getDate(timestamp))
+
+    fun unixToTime(timestamp: Int): String = timeFormatter.format(getDate(timestamp))
+
+    fun unixToPrecisionDate(timestamp: Int?, precision: DatePrecision): String =
+        timestamp?.let {
+            val formatter = when (precision) {
+                DatePrecision.DAY, DatePrecision.HOUR -> dayDateFormatter
+                DatePrecision.MONTH -> monthDateFormatter
+                else -> yearDateFormatter
+            }
+            formatter.format(getDate(timestamp))
+        } ?: "Pending"
+
+    fun unixToPrecisionTime(timestamp: Int?, precision: DatePrecision): String =
+        if (timestamp != null && precision == DatePrecision.HOUR) {
+            precisionTimeFormatter.format(getDate(timestamp))
+        } else "Pending"
 }
