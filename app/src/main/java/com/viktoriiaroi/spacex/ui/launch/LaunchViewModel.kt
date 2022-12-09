57bc6da3ac32c6ab9ac82476eecf1cb6173bd9f1
VictoriyaRoy <victoriyaroi2003@gmail.com>
2022-12-09 21:45:22 +0200

    Implement pagination on UI

--- app/src/main/java/com/viktoriiaroi/spacex/ui/launch/LaunchViewModel.kt
+++ app/src/main/java/com/viktoriiaroi/spacex/ui/launch/LaunchViewModel.kt
@@ -1,79 +1,73 @@
 package com.viktoriiaroi.spacex.ui.launch
 
 import androidx.lifecycle.viewModelScope
-import com.viktoriiaroi.core.model.Launch
 import com.viktoriiaroi.core.repository.LaunchRepository
 import com.viktoriiaroi.spacex.ui.common.BaseViewModel
 import com.viktoriiaroi.spacex.ui.launch.LaunchReducers.Companion.reduce
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@ExperimentalCoroutinesApi
 @HiltViewModel
 class LaunchViewModel @Inject constructor(private val launchRepo: LaunchRepository) :
     BaseViewModel<LaunchState, LaunchIntent>() {
-    private var launchType = MutableStateFlow(LaunchType.ALL)
+
+    private var typeFlow = MutableStateFlow(LaunchType.ALL)
+    private var launchesFlow = typeFlow.flatMapLatest { type ->
+        mState.postValue(LaunchState.LoadingFirst)
+        typeChanged = true
+        loadLaunches(1)
+        getFlowByType(type)
+    }
+
+    private var nextPage: Int? = null
+    private var typeChanged = false
+    val isTypeChanged = typeChanged.also {
+        typeChanged = false
+    }
 
     init {
-        handleIntent(LaunchIntent.LoadLaunches(LaunchType.ALL))
-        viewModelScope.launch {
-            setupAllFlow()
-        }
-        viewModelScope.launch {
-            setupPastFlow()
-        }
-        viewModelScope.launch {
-            setupFutureFlow()
+        handleIntent(LaunchIntent.ChangeType(LaunchType.ALL))
+        viewModelScope.launch(Dispatchers.Default) {
+            launchesFlow.collect {
+                mState.postValue(it.reduce())
+            }
         }
     }
 
-    private suspend fun setupAllFlow() {
-        launchRepo.allLaunchFlow.combine(launchType) { launches, type -> Pair(launches, type) }
-            .collect {
-                if (it.second == LaunchType.ALL) {
-                    postResult(it.first)
-                }
-            }
-    }
-
-    private suspend fun setupPastFlow() {
-        launchRepo.pastLaunchFlow.combine(launchType) { launches, type -> Pair(launches, type) }
-            .collect {
-                if (it.second == LaunchType.PAST) {
-                    postResult(it.first)
-                }
-            }
-    }
-
-    private suspend fun setupFutureFlow() {
-        launchRepo.futureLaunchFlow.combine(launchType) { launches, type -> Pair(launches, type) }
-            .collect {
-                if (it.second == LaunchType.FUTURE) {
-                    postResult(it.first)
-                }
-            }
-    }
-
-    private fun postResult(result: Result<List<Launch>>) {
-        mState.postValue(result.reduce())
-    }
+    private fun getFlowByType(type: LaunchType) =
+        when (type) {
+            LaunchType.ALL -> launchRepo.allLaunchFlow
+            LaunchType.PAST -> launchRepo.pastLaunchFlow
+            LaunchType.FUTURE -> launchRepo.futureLaunchFlow
+        }
 
     override fun handleIntent(intent: LaunchIntent) {
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.Default) {
             when (intent) {
-                is LaunchIntent.LoadLaunches -> loadLaunches(intent.launchType)
+                is LaunchIntent.ChangeType -> typeFlow.emit(intent.launchType)
+                is LaunchIntent.LoadNextPage -> loadNextPage()
             }
         }
     }
 
-    private suspend fun loadLaunches(newType: LaunchType) {
-        launchType.value = newType
-        when (newType) {
-            LaunchType.ALL -> launchRepo.updateAllLaunches()
-            LaunchType.PAST -> launchRepo.updatePastLaunches()
-            LaunchType.FUTURE -> launchRepo.updateFutureLaunches()
+    private suspend fun loadNextPage() {
+        nextPage?.let {
+            mState.postValue(LaunchState.LoadingNext)
+            loadLaunches(it)
+        }
+    }
+
+    private suspend fun loadLaunches(page: Int) {
+        nextPage = when (typeFlow.value) {
+            LaunchType.ALL -> launchRepo.updateAllLaunches(page)
+            LaunchType.PAST -> launchRepo.updatePastLaunches(page)
+            LaunchType.FUTURE -> launchRepo.updateFutureLaunches(page)
         }
     }
 }
